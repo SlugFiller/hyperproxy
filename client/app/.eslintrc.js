a2b473cafcf6656ca5d387650f8fb381601515d8
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-08-22 13:33:11 +0300

    Bip39 autocomplete

--- client/app/.eslintrc.js
+++ client/app/.eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+          project: "./tsconfig.json",
+          ecmaVersion: "latest",
+          sourceType: "module",
+      },
+    },
+  ],
 };
--- client/app/App.tsx
+++ client/app/App.tsx
@@ -5,41 +5,87 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
+	wordlist
+} from '@scure/bip39/wordlists/english';
+import {
+	useMemo,
+	useState,
+} from 'react';
+import {
+	Pressable,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+} from 'react-native';
+import AutoComplete from 'react-native-autocomplete-input';
+import {
+	SafeAreaProvider,
+	SafeAreaView,
 } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+	return (
+		<SafeAreaProvider>
+			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+			<AppContent />
+		</SafeAreaProvider>
+	);
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+	const [word, setWord] = useState<string>('');
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Bip39Word
+				value={word}
+				setValue={setWord}
+			/>
+		</SafeAreaView>
+	);
+}
+
+function Bip39Word({ value, setValue, ...props }: { value: string, setValue: React.Dispatch<React.SetStateAction<string>> }) {
+	const data = useMemo<string[]>(() => {
+		if (!value.length) {
+			return [];
+		}
+		const ret: string[] = wordlist.filter((word) => word.startsWith(value));
+		if (ret.length === 1 && ret[0] === value) {
+			return [];
+		}
+		return ret;
+	}, [value]);
+
+	return (
+		<AutoComplete
+			data={data}
+			value={value}
+			onChangeText={setValue}
+			flatListProps={{
+				renderItem: ({ item }: { item: string }) => (
+					<Pressable onPress={() => setValue(item)}>
+						<Text style={styles.autoCompleteItem}>{item}</Text>
+					</Pressable>
+				),
+			}}
+			{ ...props }
+		/>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
+	autoCompleteItem: {
+		fontSize: 15,
+		padding: 5,
+	},
 });
 
 export default App;
